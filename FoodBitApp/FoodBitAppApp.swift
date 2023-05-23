8154a247c6cbe64b08c6371ac7b52830375807ff
mohammed alsaad <mohaalsaad.j@gmail.com>
2023-05-23 10:34:20 +0300

    first commit

---
+++ FoodBitApp/FoodBitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodBitAppApp.swift
+//  FoodBitApp
+//
+//  Created by mohammed alsaad on 23/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodBitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
